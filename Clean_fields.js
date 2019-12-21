b40fd8768b04b8673d9012520b5a3fb3ef558bfc
giorgiob88na <58940735+giorgiob88na@users.noreply.github.com>
2019-12-21 13:46:45 +0100

    Create Clean_fields.js

---
+++ Clean_fields.js
@@ -0,0 +1,17 @@
+// This script cleans fields like textboxes
+// It could be useful on reload of a page
+
+<script>
+function clean(){
+	//alert('Hai premuto il tasto BACK!!');
+	var x=documentGetElementById('nomecerca').value;
+	var y=documentGetElementById('cognomecerca').value;
+	var z=documentGetElementById('marcacerca').value;
+	var t=documentGetElementById('modellocerca').value;
+	x=" ";
+	y=" ";
+	z=" ";
+	t=" ";
+}
+
+</script>
